90c5886f3b74acd8ec1ad137969b1e7ce734b8d4
bete7512 <betekbebe@gmail.com>
2023-06-25 11:18:10 +0300

    changes

--- express/src/utility/user.js
+++ express/src/utility/user.js
@@ -1,8 +1,17 @@
 import client from "../configuration/apollo.config";
-const QUERY_BY = ``
+const QUERY_BY = `
+query MyQuery($adress_id: uuid = "") {
+    addresses_by_pk(adress_id: $adress_id) {
+      address_line_one
+    }
+  }
+  `
+  User({
+    adress_id: 1
+})
 const User = async (variables) => {
     const data = await client.request(QUERY_BY, variables);
-    return data['users_by_pk'];
+    return data['addresses_by_pk']['address_line_one'];
     }
 
 
--- hasura/migrations/1687554487704_alter_table_public_business_reviews_drop_column_test/down.sql
+++ hasura/migrations/1687554487704_alter_table_public_business_reviews_drop_column_test/down.sql
@@ -1,2 +1,3 @@
 alter table "public"."business_reviews" alter column "test" drop not null;
 alter table "public"."business_reviews" add column "test" text;
+jkncksdbm
