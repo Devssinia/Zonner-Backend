6742e48edb005f2bc8d2c3370c611d33522cee6c
bete7512 <betekbebe@gmail.com>
2023-06-26 20:56:08 +0300

    Backend on the way

---
+++ hasura/migrations/1687801772298_alter_table_public_customers_alter_column_phone_no/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."customers" drop constraint "customers_phone_no_key";
---
+++ hasura/migrations/1687801772298_alter_table_public_customers_alter_column_phone_no/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."customers" add constraint "customers_phone_no_key" unique ("phone_no");
