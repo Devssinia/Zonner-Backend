d9cd36495ebcb28eb4e0404b32d6f9e2f9b0d52b
bete7512 <betekbebe@gmail.com>
2023-08-01 05:48:39 +0300

    Do things using absolute location instead of relative

--- set_up_dump.sh
+++ set_up_dump.sh
@@ -4,4 +4,4 @@
 source ~/Zonner-Backend/.env
 
 # Execute the dumped.sql file with the password from the password file
-PGPASSWORD="$DB_ZONNER_ADMIN_PASSWORD" psql "dbname=zonner_db user=zonner_db_admin" -f ./dumped.sql
+PGPASSWORD="$DB_ZONNER_ADMIN_PASSWORD" psql "dbname=zonner_db user=zonner_db_admin" -f ~/Zonner-Backend/dumped.sql
