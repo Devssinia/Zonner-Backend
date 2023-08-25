5a3f31ff29ab6421c7a84c058452cc3a2af39591
aderawmolla <mollaaderaw@gmail.com>
2023-08-25 20:34:18 +0300

    handle file

--- express/actions/file_upload.js
+++ express/actions/file_upload.js
@@ -1,42 +1,18 @@
 import cloudinary from '../configuration/cloudinary.js';
-import { createReadStream } from 'fs'; // Import createReadStream directly from 'fs' module
 
 const file_upload = async (req, res) => {
-  const filePath = req.body.path;
-  console.log(`the path of the file is ${filePath}`);
-
   try {
-    // Read the file asynchronously using streams
-    const readStream = createReadStream(filePath); // Use createReadStream directly
-
-    // Upload the file to Cloudinary
-    const uploadOptions = {
-      resource_type: 'auto',
-      public_id: 'zonner-files',
-    };
-
-    const uploadResult = await cloudinary.uploader.upload_stream(
-      uploadOptions,
-      (error, result) => {
-        if (error) {
-          console.error(error);
-          return res.status(400).json({
-            message: error.message || 'An error occurred during upload',
-          });
-        }
-
-        return res.json({
-          file_url: result.url,
-        });
-      }
-    );
-
-    // Pipe the file data to the Cloudinary uploader
-    readStream.pipe(uploadResult);
+    const {file_type,base64str } = req.body;
+    // Upload the base64-encoded file content to Cloudinary
+    const result = await cloudinary.uploader.upload(`data:${file_type};base64,${base64str}`);
+    
+    return res.json({
+      file_url: result.url
+    });
   } catch (e) {
-    console.error(e);
+    console.log(e);
     return res.status(400).json({
-      message: e.message || 'An error occurred',
+      message: e
     });
   }
 };
