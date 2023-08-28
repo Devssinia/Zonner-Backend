89881a19688a5b1eb419400158d8074441cbf89b
aderawmolla <mollaaderaw@gmail.com>
2023-08-28 11:42:37 +0300

    file input

--- express/actions/file_upload.js
+++ express/actions/file_upload.js
@@ -2,19 +2,18 @@ import cloudinary from '../configuration/cloudinary.js';
 
 const file_upload = async (req, res) => {
   try {
-    const {file_type,base64str } = req.body;
+    const {file_type,base64str}=req.body.input;
     // Upload the base64-encoded file content to Cloudinary
     const result = await cloudinary.uploader.upload(`data:${file_type};base64,${base64str}`);
-    
+
     return res.json({
       file_url: result.url
     });
-  } catch (e) {
+  } catch (e){
     console.log(e);
     return res.status(400).json({
       message: e
     });
   }
 };
-
 export { file_upload };
