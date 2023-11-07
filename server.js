9ec463e42ca15b3b5a9f57342fb8b2e90ed273fb
aderawmolla <mollaaderaw@gmail.com>
2023-11-07 13:44:41 +0300

    port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: '200mb' }))
 app.use(cors())
 app.use('/', routes) 
 const port = process.env.EXPRESS_PORT;
-app.listen( port||3000, () => {
+app.listen( port||2001, () => {
   console.log(`server started successfully ${port}`)
 })
 // AWS Aurora Serverless cluster.
