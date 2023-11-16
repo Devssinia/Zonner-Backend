a29176e686c231c14c780f64a5675feb02667cb3
aderawmolla <mollaaderaw@gmail.com>
2023-11-16 07:30:36 +0300

    transaction id

--- express/actions/payments/c_to_b_payment.js
+++ express/actions/payments/c_to_b_payment.js
@@ -55,11 +55,11 @@ exports.payAmount=async(req,res)=>{
             }
         })
      console.log(data)
-    //   const transaction= await  insert_transaction({phone_number:phoneNumber,amount: amount,transaction_date:Date(timestamp) ,mpesa_transaction_id:data["CheckoutRequestID"], status:"unpaid",order_id:order_id})
-    //   console.log("the transaction is",transaction)
+      const transaction= await  insert_transaction({phone_number:phoneNumber,amount: amount,transaction_date:Date(timestamp) ,mpesa_transaction_id:data["CheckoutRequestID"], status:"unpaid",order_id:order_id})
+      console.log("the transaction is",transaction)
      return  res.status(200).json({     
         "mpessa_transaction_id":data["CheckoutRequestID"],
-        // "transaction_id":transaction
+        "transaction_id":transaction
     })
     }catch(err){
         console.log(err);
