30bfd80cdc6581f6541edafb7fea139a2b6e095a
aderawmolla <mollaaderaw@gmail.com>
2024-03-11 19:01:33 +0300

    adding address to hasura console

---
+++ hasura/migrations/default/1709882425291_alter_table_public_businesses_add_column_address_name/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."businesses" add column "address_name" text
+--  null default 'Kenya,Nairobi';
---
+++ hasura/migrations/default/1709882425291_alter_table_public_businesses_add_column_address_name/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."businesses" add column "address_name" text
+ null default 'Kenya,Nairobi';
